875b48d5d7788aff1ceea87b7d1e19c8d4110ddc
Alexey Frolov <gartenzius@gmail.com>
2019-03-03 12:57:13 +0300

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!([{ name: 'Bob', role: 'User' }, { name: 'Maggie', role: 'User' }])
+
+Category.create!([{ title: 'backend' }, { title: 'frontend' }, { title: 'administration' }])
+
+Test.create!(title: 'Ruby', level: 1, category_id: Category.find_by(title: 'backend').id)
+Test.create!(title: 'Rails', level: 2, category_id: Category.find_by(title: 'frontend').id)
+Test.create!(title: 'CSS', level: 1, category_id: Category.find_by(title: 'frontend').id)
+Test.create!(title: 'HTML', level: 1, category_id: Category.find_by(title: 'frontend').id)
+Test.create!(title: 'C#', level: 1, category_id: Category.find_by(title: 'backend').id)
+Test.create!(title: 'C++', level: 1, category_id: Category.find_by(title: 'backend').id)
+
+Question.create!(body: 'question1', test_id: Test.find_by(title: 'Ruby').id)
+Question.create!(body: 'question1', test_id: Test.find_by(title: 'CSS').id)
+
+Answer.create!(body: "False answer", question_id: Question.where("body = :body AND test_id = :id", 'question1', Test.find_by(title: 'Ruby').id))
+Answer.create!(body: "False answer", question_id: Question.where("body = :body AND test_id = :id", 'question1', Test.find_by(title: 'Ruby').id))
+Answer.create!(body: "False answer", question_id: Question.where("body = :body AND test_id = :id", 'question1', Test.find_by(title: 'Ruby').id))
+Answer.create!(body: "True answer", correct: true, question_id: Question.where("body = :body AND test_id = :id", 'question1', Test.find_by(title: 'Ruby').id))
+
+Answer.create!(body: "False answer", question_id: Question.where("body = :body AND test_id = :id", 'question1', Test.find_by(title: 'CSS').id))
+Answer.create!(body: "False answer", question_id: Question.where("body = :body AND test_id = :id", 'question1', Test.find_by(title: 'CSS').id))
+Answer.create!(body: "False answer", question_id: Question.where("body = :body AND test_id = :id", 'question1', Test.find_by(title: 'CSS').id))
+Answer.create!(body: "True answer", correct: true, question_id: Question.where("body = :body AND test_id = :id", 'question1', Test.find_by(title: 'CSS').id))
