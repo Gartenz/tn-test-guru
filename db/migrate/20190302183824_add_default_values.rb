dd629da75a71ac3490390cb69ee1c0353b98ab12
Alexey Frolov <Gartenz@users.noreply.github.com>
2019-03-03 12:12:03 +0300

    Update 20190302183824_add_default_values.rb

--- db/migrate/20190302183824_add_default_values.rb
+++ db/migrate/20190302183824_add_default_values.rb
@@ -1,6 +1,6 @@
 class AddDefaultValues < ActiveRecord::Migration[5.2]
   def change
     change_column_default(:tests, :level, 0)
-    change_column_default(:answers, :right, false)
+    change_column_default(:answers, :correct, false)
   end
 end
