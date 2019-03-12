fe025c7f2d9546d57845e58b5dff1e6378923423
Alexey Frolov <gartenzius@gmail.com>
2019-03-12 19:28:03 +0300

    add test passage

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -5,9 +5,34 @@ class TestPassage < ApplicationRecord
 
   before_validation :before_validation_set_first_question, on: :create
 
+  def completed?
+    current_question.nil?
+  end
+
+  def accept!(answer_ids)
+    if correct_answer?(answer_ids)
+      self.correct_questions += 1
+    end
+
+    self.current_question = next_question
+    save!
+  end
+
   private
 
+  def next_question
+    test.questions.order(:id).where('id > ?', current_question.id).first
+  end
+
+  def correct_answer?(answer_ids)
+    correct_answers.ids.sort == answer_ids.map(&:to_i).sort
+  end
+
+  def correct_answers
+    current_question.answers.correct
+  end
+
   def before_validation_set_first_question
-    current_question = test.questions.first if test.persisted?
+    self.current_question = test.questions.first if test.persisted?
   end
 end
