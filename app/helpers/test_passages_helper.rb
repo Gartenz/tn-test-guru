9c4d8c4708d7c648c2e9731ca625ce4bf1fe651b
Alexey Frolov <gartenzius@gmail.com>
2019-03-14 21:54:33 +0300

    Add fix

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -1,6 +1,7 @@
 module TestPassagesHelper
-  def result_test_title(result, title)
-    result ? "Congratulations! You are passed test #{title}" :
-             "Sorry! You are not passed test #{title}"
+  def result_test_title(test_passage)
+    title = test_passage.test.title
+    test_passage.succes? ? "Congratulations! You are passed test #{title}" :
+                           "Sorry! You are not passed test #{title}"
   end
 end
