c4092084855c57a32acc82cae2b1d1bdfd26f947
Alexey Frolov <gartenzius@gmail.com>
2019-03-18 01:39:09 +0300

    Add HW12

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :set_request_path
-  before_action :display_welcome_flash, if: :user_signed_in?
+  # before_action :display_welcome_flash, if: :user_sign1ed_in?
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
+
+  def default_url_options
+    default_lang? ? {} : { lang: I18n.locale }
+  end
 
   protected
 
@@ -10,7 +15,16 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
   end
 
+
   private
+
+  def default_lang?
+    I18n.default_locale == I18n.locale
+  end
+
+  def set_locale
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
+  end
 
   def display_welcome_flash
     flash[:welcome] = "Привет, #{current_user.first_name}!"
--- config/application.rb
+++ config/application.rb
@@ -10,10 +10,11 @@ module TestGuru
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    
+
     config.i18n.default_locale = :ru
     config.i18n.fallbacks = [:en]
-
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    
     config.time_zone = 'Moscow'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
