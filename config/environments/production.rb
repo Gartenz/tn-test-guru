598b18048aba5a0d4d8302e363c424cca07974b1
Alexey Frolov <gartenzius@gmail.com>
2019-03-26 23:06:29 +0300

    most stupid commit ever

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'gartenz-testguru.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.yandex.ru'
+    address:              'smtp.yandex.ru',
     port:                 587,
     user_name:            ENV['SMTP_LOGIN'],
     password:             ENV['SMTP_PASSWD'],
